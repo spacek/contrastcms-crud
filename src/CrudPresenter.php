199c1b8d523b32a82c6e7cca20a507314b35cd22
Jiri Spacek <jiri@spacek.org>
2018-09-03 18:20:49 +0200

    thumbnails

--- src/CrudPresenter.php
+++ src/CrudPresenter.php
@@ -512,7 +512,7 @@ class CrudPresenter extends SecuredPresenter
 			$fileRepository = $this->context->getService("fileRepository");
 			$fileType = $fileRepository->getFileType($fileId);
 			if($fileType === "image") {
-				return $fileRepository->getFilenameResized($fileId, 100);
+				return '<img src="'. $fileRepository->getFilenameResized($fileId, 100) .'" />';
 			} else {
 				$path = "/data/" . $fileRepository->getFilename($fileId);
 				return '<a target="_blank" href="'. $path .'">Open file</a>';
